f1bea2e542136515613a8b557826505b067da25e
zaw myo naing <zawmyonaing@cgm-myanmar.com>
2025-10-02 21:06:00 +0630

    assignment 2 updated

--- src/main/java/Assignment2/AssignmentObjects.java
+++ src/main/java/Assignment2/AssignmentObjects.java
@@ -2,6 +2,8 @@ package main.java.Assignment2;
 
 public class AssignmentObjects {
     public static void main(String[] args) {
+
         System.out.println("Assigment 2");
+        System.out.println("Assigment 2 updated");
     }
 }
